77469233d402e28d94c93a208b0f0eba7386d35e
tqer39 <tqer39@gmail.com>
2024-12-07 00:19:55 +0900

    --copy-source-raw-directoryオプションを--directoryに変更し、引数の処理を更新

--- 01_file_copy.py
+++ 01_file_copy.py
@@ -19,7 +19,7 @@ def parse_arguments() -> Namespace:
 
     # [OPTION] 引数
     parser.add_argument(
-        "--copy-source-raw-directory",
+        "--directory",
         help="[OPTION] 元になる音声ファイル（mp3, wav など）のパスを指定するディレクトリ",
     )
     parser.add_argument(
@@ -37,17 +37,17 @@ def main(args=None):
     if args is None:
         args = parse_arguments()
 
-    if args.copy_source_raw_directory:
+    if args.directory:
         raw_dir = os.path.join(f"./data/{args.model_name}", "raw")
         os.makedirs(raw_dir, exist_ok=True)
-        for file in os.listdir(args.copy_source_raw_directory):
+        for file in os.listdir(args.directory):
             if file.endswith((".mp3", ".wav")):
                 dest_file = os.path.join(raw_dir, file)
                 if os.path.exists(dest_file) and not args.force:
                     print(f"スキップされたファイル: {dest_file}（既に存在します）")
                 else:
                     shutil.copy(
-                        os.path.join(args.copy_source_raw_directory, file), dest_file
+                        os.path.join(args.directory, file), dest_file
                     )
                     print(f"コピーされたファイル: {dest_file}")
 
