a8a534715d8d8a52f547ff0581cfb646fddc29a3
tqer39 <tqer39@gmail.com>
2024-12-07 21:08:59 +0900

    08_training.pyを追加し、モデルのファインチューニング用のコマンドライン引数を解析する機能を実装

---
+++ 08_training.py
@@ -0,0 +1,66 @@
+import subprocess
+import os
+from typing import Optional
+import sys
+import argparse
+from argparse import Namespace
+
+
+def parse_arguments() -> argparse.Namespace:
+    """
+    コマンドライン引数を解析します。
+    """
+    parser = argparse.ArgumentParser(
+        description="Fine-tuning script for GenLipSyncVideo"
+    )
+    parser.add_argument(
+        "--model-name", "-M", type=str, help="Name of the model to fine-tune"
+    )
+    parser.add_argument(
+        "--directory",
+        "-D",
+        type=str,
+        help="[REQUIRED] YYMMDD_HHMMSS フォーマットのディレクトリ名を指定します。",
+    )
+    return parser.parse_args()
+
+
+def training(project: str) -> None:
+    """
+    モデルをトレーニングします。
+    """
+    os.environ["HYDRA_FULL_ERROR"] = "1"  # エラー詳細を表示する
+
+    command = [
+        ".venv/Scripts/python",
+        "fish_speech/train.py",
+        "--config-name",
+        "text2semantic_finetune",
+        f"project={project}",
+        "+lora@model.model.lora_config=r_8_alpha_16",
+    ]
+    subprocess.run(command, check=True)
+
+
+def main(args: Optional[Namespace] = None) -> None:
+    """
+    メイン関数。コマンドライン引数を解析し、fine tuning の処理を実行します。
+    """
+    if args is None:
+        args = parse_arguments()
+
+    model_name = os.getenv("MODEL_NAME") or args.model_name
+    if not model_name:
+        print("モデル名が指定されていません。")
+        return
+
+    directory = os.getenv("FS_DATA_TS") or args.directory
+    if not directory:
+        print("ディレクトリが指定されていません。")
+        return
+
+    training(model_name)
+
+
+if __name__ == "__main__":
+    main()
