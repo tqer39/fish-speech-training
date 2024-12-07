7593c065a6e86fcd7f04f53f7c779fa99d787743
tqer39 <tqer39@gmail.com>
2024-12-07 10:09:04 +0900

    コマンドライン引数にモデル名オプションを追加し、引数の整形を改善

--- 05_create_protobuf.py
+++ 05_create_protobuf.py
@@ -4,27 +4,31 @@ import argparse
 from argparse import Namespace
 from typing import Optional
 
+
 def parse_arguments() -> argparse.Namespace:
     """
     コマンドライン引数を解析します。
     """
     parser = argparse.ArgumentParser(description="protobuf ファイルを生成します。")
+    parser.add_argument("--model-name", "-M", type=str, help="モデル名を指定します。")
     parser.add_argument(
-        "--model-name", "-M", type=str, help="モデル名を指定します。"
-    )
-    parser.add_argument(
-        "--force", "-F",
+        "--force",
+        "-F",
         action="store_true",
         help="[OPTION] 既存の protos ファイルがある場合に強制的に上書きします。",
     )
     parser.add_argument(
-        "--directory", "-D", required=True, help="[REQUIRED] YYMMDD_HHMMSS フォーマットのディレクトリ名を指定します。"
+        "--directory",
+        "-D",
+        required=True,
+        help="[REQUIRED] YYMMDD_HHMMSS フォーマットのディレクトリ名を指定します。",
     )
     parser.add_argument(
         "--override-path",
         help="[OPTION] 処理対象のディレクトリを指定します。デフォルトは './data/{model_name}/raw/{directory}/before_text_reformatting' です。",
     )
     return parser.parse_args()
+
 
 def create_protobuf(input_dir: str, output_dir: str, force: bool) -> None:
     """
@@ -51,6 +55,7 @@ def create_protobuf(input_dir: str, output_dir: str, force: bool) -> None:
     ]
     subprocess.run(command, check=True)
 
+
 def main(args: Optional[Namespace] = None) -> None:
     """
     メイン関数。コマンドライン引数を解析し、protobuf ファイルを生成します。
@@ -64,11 +69,13 @@ def main(args: Optional[Namespace] = None) -> None:
         return
 
     target_dir = (
-        args.override_path or f"./data/{model_name}/raw/{args.directory}/before_text_reformatting"
+        args.override_path
+        or f"./data/{model_name}/raw/{args.directory}/before_text_reformatting"
     )
     output_dir = f"./data/{model_name}/raw/{args.directory}/protobuf"
 
     create_protobuf(target_dir, output_dir, args.force)
+
 
 if __name__ == "__main__":
     main()
