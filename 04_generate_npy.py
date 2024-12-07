28f9ca95552ac42157f9d3fe6d8082335e1d7088
tqer39 <tqer39@gmail.com>
2024-12-07 09:25:26 +0900

    モデル名の引数を追加し、出力ディレクトリを指定するように修正

--- 04_generate_npy.py
+++ 04_generate_npy.py
@@ -7,15 +7,16 @@ def parse_arguments():
     コマンドライン引数を解析します。
     """
     parser = argparse.ArgumentParser(description="音声データから npy ファイルを生成します。")
-    parser.add_argument('--directory', '-D', type=str, required=True, help='入力音声ファイルが含まれるディレクトリのパス')
+    parser.add_argument('--directory', '-D', type=str, required=True, help='入力音声��ァイルが含まれるディレクトリのパス')
+    parser.add_argument('--model-name', '-M', help='モデル名（環境変数 MODEL_NAME からも読み取ります）')
     parser.add_argument('--checkpoint-path', type=str, help='チェックポイントファイルのパス (環境変数 FS_CHECKPOINT_PATH が優先されます)')
     return parser.parse_args()
 
-def generate_npy(input_file: str, checkpoint_path: str):
+def generate_npy(input_file: str, output_dir: str, checkpoint_path: str):
     """
     npy ファイルを生成します。
     """
-    output_file = os.path.splitext(input_file)[0] + '.npy'
+    output_file = os.path.join(output_dir, os.path.splitext(os.path.basename(input_file))[0] + '.npy')
     command = [
         'python', 'tools/vqgan/inference.py',
         '-i', input_file,
@@ -28,17 +29,25 @@ def main():
     args = parse_arguments()
 
     directory = args.directory
+    model_name = os.getenv("MODEL_NAME") or args.model_name
     checkpoint_path = os.getenv('FS_CHECKPOINT_PATH') or args.checkpoint_path
+
+    if not model_name:
+        print("モデル名が指定されていません。")
+        return
 
     if not checkpoint_path:
         print("チェックポイントファイルのパスが指定されていません。")
         return
 
+    output_dir = os.path.join(f"./data/{model_name}/raw/{directory}/npy")
+    os.makedirs(output_dir, exist_ok=True)
+
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(('.wav', '.mp3')):
                 input_file = os.path.join(root, file)
-                generate_npy(input_file, checkpoint_path)
+                generate_npy(input_file, output_dir, checkpoint_path)
 
 if __name__ == "__main__":
     main()
